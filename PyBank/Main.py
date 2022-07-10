453a6412c8d7a7e30bc620e1f316a9a3d2126f55
Jacob Douthett <jadouth@gmail.com>
2022-07-10 15:18:53 -0400

    Added PyPoll files and script

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -10,12 +10,12 @@ with open(csvpath) as csvfile:
     print(csvreader)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-    for row in csvreader:
-        print(row)
+    #for row in csvreader:
+       # print(row)
 
 #Print to a new CSV file
 output_path = os.path.join('Analysis', "PyBank_Analysis")
 with open(output_path, 'w') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
     csvwriter.writerow(['Date', " Profit/Losses"])
-    csvwriter.writerow(['Today', " A Lot"])
+    csvwriter.writerow(['Today', " not enough"])
