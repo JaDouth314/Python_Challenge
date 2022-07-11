78c51996407197c314dd59171e4d0b8c808b4d44
Jacob Douthett <jadouth@gmail.com>
2022-07-11 13:44:53 -0400

    In-progress work on part2 of homework 3

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -3,6 +3,10 @@ import os
 import csv
 
 csvpath = os.path.join('Resources', 'election_data.csv')
+VoteTotal = []
+
+
+
 
 #Open the CSV file
 with open(csvpath) as csvfile:
@@ -10,12 +14,21 @@ with open(csvpath) as csvfile:
     print(csvreader)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-    for row in csvreader:
-        print(row)
+    # for row in csvreader:
+    #     print(row)
 
-#Print to a new CSV file
+    for row in (csvfile):
+        VoteTotal == 0
+        VoteTotal.append(row[0])
+        
+    print(len(VoteTotal))
+
+    #Print to a new CSV file
 output_path = os.path.join('Analysis', "PyPoll_Analysis")
 with open(output_path, 'w') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
-    csvwriter.writerow(['Ballot', " County", " Candidate"])
-    csvwriter.writerow(['100', " Jefferson", " Me"])
+    csvfile.write(f"Election Results")
+    csvfile.write("\n")
+    csvfile.write(f"----------------")
+    csvfile.write("\n")
+    csvfile.write(f"The total number of votes was: {len(VoteTotal)}")
